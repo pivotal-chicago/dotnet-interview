d52dc1d9a6d0a3ba2541711c67fdb754349a2f16
Jon Blackburn <jblackburn@pivotal.io>
2018-04-30 06:56:43 -0700

    Add test for deleting an Address

--- XUnitTestProject1/Model/FraudulentAddressTests.cs
+++ XUnitTestProject1/Model/FraudulentAddressTests.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using FraudDomain.Model;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -29,6 +28,9 @@ namespace FraudDomain
             };
 
             Assert.Equal(address, address2);
+
+            address2.Street = "Maple Avenue";
+            Assert.NotEqual(address, address2);
         }
 
         [Fact]
@@ -49,13 +51,38 @@ namespace FraudDomain
             {
                 var allAddresses = db.Addresses;
                 Assert.Empty(allAddresses);
-                
+
                 allAddresses.Add(address);
                 db.SaveChanges();
 
                 allAddresses = db.Addresses;
                 Assert.Single(allAddresses);
                 Assert.All(allAddresses, addressFromDb => Assert.Equal(address, addressFromDb));
+            }
+        }
+
+        [Fact]
+        public void ShouldDeleteAddress()
+        {
+            var address = new FraudulentAddress
+            {
+                Street = "Main Street",
+                StreetNumber = "123",
+                City = "Chicago",
+                State = "IL",
+                ZIP = "60001"
+            };
+
+            var builder = new DbContextOptionsBuilder<FraudulentAddressContext>().UseInMemoryDatabase("unitTestDb");
+
+            using (var db = new FraudulentAddressContext(builder.Options))
+            {
+                db.Addresses.Add(address);
+                db.SaveChanges();
+                Assert.Single(db.Addresses);
+                db.Remove(address);
+                db.SaveChanges();
+                Assert.Empty(db.Addresses);
             }
         }
     }
