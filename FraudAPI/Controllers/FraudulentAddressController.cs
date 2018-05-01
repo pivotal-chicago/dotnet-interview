6ea0b4fd204dcbe1c8c1829710119c140be414fc
Jon Blackburn <jblackburn@pivotal.io>
2018-05-01 11:41:34 -0700

    Clean up controller (comments, etc.)

--- FraudAPI/Controllers/FraudulentAddressController.cs
+++ FraudAPI/Controllers/FraudulentAddressController.cs
@@ -16,35 +16,30 @@ namespace FraudAPI.Controllers
             this.service = service;
         }
 
-        // GET: api/FraudulentAddress
         [HttpGet]
         public IEnumerable<FraudulentAddress> Get()
         {
             return service.All();
         }
 
-        // GET: api/FraudulentAddress/5
         [HttpGet("{id}", Name = "Get")]
         public FraudulentAddress Get(int id)
         {
             return service.Find(id);
         }
         
-        // POST: api/FraudulentAddress
         [HttpPost]
         public void Post([FromBody]FraudulentAddress value)
         {
             service.Save(value);
         }
         
-        // PUT: api/FraudulentAddress/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]FraudulentAddress value)
         {
             service.Save(value);
         }
         
-        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
